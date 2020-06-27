ec953e3dd50cf0c21ed4dc11ead2bc5a628d067f
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-27 09:27:42 +0900

    Create the first view

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,4 @@
-"""config URL Configuration
+"""polls URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,16 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-]
+from . import views
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    path('', views.index, name='index'),
+]
