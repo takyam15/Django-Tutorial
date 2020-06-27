91a20274b1f5149834207d23508e1d952e1cd37f
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-27 09:30:31 +0900

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -12,8 +12,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.PROTECT)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
