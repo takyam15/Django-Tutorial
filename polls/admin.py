c5f5f39d954c0516bfa58ab85574840adbd225ef
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-27 09:15:31 +0900

    Create Question and Choice models

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import User
+from .models import (
+    User, Question, Choice
+)
 
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -6,3 +6,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.PROTECT)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
