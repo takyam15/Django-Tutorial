bd2acfa43b7ab8c6251f3127972ac09934ba6d91
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-27 09:48:59 +0900

    Add basic views, templates, and urls

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -11,6 +14,9 @@ class User(AbstractUser):
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text
--- polls/urls.py
+++ polls/urls.py
@@ -17,6 +17,10 @@ from django.urls import include, path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('results/<int:question_id>/', views.results, name='results'),
+    path('vote/<int:question_id>/', views.vote, name='vote'),
 ]
